b3ef96be6f303e3aedc9ca4d3b7dca4d046001e8
scubao <oharwardt@googlemail.com>
2015-12-29 15:59:24 +0100

    AddItem

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ app.controller('getShoppingEntries', ['$scope', '$http', function ($scope, $http
     $http.get('http://127.0.0.1:8080/entries').success(function (data) {
         $scope.data = data;
         $scope.save = function(entry) {
-            $scope.message = "Save " + entry.done
+            $scope.message = "Save " + entry.done;
+        }
+        $scope.checkEnter = function($event) {
+            var keyCode = $event.which || $event.keyCode;
+            if (keyCode == 13) {
+                $scope.newproduct = $scope.hanspeter;
+            }
         }
     });
 }]);
